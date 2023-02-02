505ecfd08ad7a9808e00122faf6c0937c4e46b70
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-02 22:14:22 +0530

    made bold and changed background color

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
 var list = document.getElementsByTagName ('li');
 for (let i=0; i<list.length; i++)
 {
-    list[i].style.color= 'green';
+    list[i].style.fontWeight= 'bold';
 }
-document.getElementById('header-title').style.border='3px solid black';
+// document.getElementById('header-title').style.border='3px solid black';
+list[2].style.backgroundColor='green'
