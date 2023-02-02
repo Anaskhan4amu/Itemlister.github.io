7e3dc0db8581a0b72961b157fc2f480ed2a05d43
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-02 22:04:58 +0530

    changed color and border

---
+++ dom.js
@@ -0,0 +1,6 @@
+var list = document.getElementsByTagName ('li');
+for (let i=0; i<list.length; i++)
+{
+    list[i].style.color= 'green';
+}
+document.getElementById('header-title').style.border='3px solid black';
