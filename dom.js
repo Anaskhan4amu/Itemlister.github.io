fcb881958f63b84215dc7cdc2af5af16ee6a350c
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-03 15:42:47 +0530

    Made the 2nd item green background color & the 3rd item invisible

--- dom.js
+++ dom.js
@@ -1,7 +1,12 @@
-var list = document.getElementsByTagName ('li');
-for (let i=0; i<list.length; i++)
-{
-    list[i].style.fontWeight= 'bold';
-}
-// document.getElementById('header-title').style.border='3px solid black';
-list[2].style.backgroundColor='green'
+// var list = document.getElementsByTagName ('li');
+// for (let i=0; i<list.length; i++)
+// {
+//     list[i].style.fontWeight= 'bold';
+// }
+// // document.getElementById('header-title').style.border='3px solid black';
+// list[2].style.backgroundColor='green'
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green'
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden'
