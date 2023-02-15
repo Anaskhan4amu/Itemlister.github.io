e60774e75582ba0cda8591ed9a115aa5c6d641ab
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-15 23:36:48 +0530

    task 13

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ function saveToLocalStorage(event) {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
+    //localStorage.setItem('name', name);
+    //localStorage.setItem('email', email);
     const obj = {name,email}
     localStorage.setItem(obj.email, JSON.stringify(obj));
-
     showNewUserOnScreen(obj);
 }
 
 function showNewUserOnScreen (user) {
+
+    document.getElementById('email').value='';
+    document.getElementById('name').value='';
+
+
     const parentNode = document.getElementById('listOfUsers');
-    const childHTML = `<li> ${user.name} - ${user.email} </li>`
+    const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
+        <button onclick=deleteUser('${user.email}')> Delete User </button> 
+        </li>`
 
     parentNode.innerHTML = parentNode.innerHTML+childHTML;
+}
+
+function deleteUser(emailId){
+    console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+
+}   
+
+function removeUserFromScreen(emailId){
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+
+    parentNode.removeChild(childNodeToBeDeleted)
 }
