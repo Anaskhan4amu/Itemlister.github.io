6a805ad8ae89362eace02b5e49c39f997082730e
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-15 22:23:25 +0530

    task 12

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ function saveToLocalStorage(event) {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-
     const obj = {name,email}
-    localStorage.setItem(obj.email, JSON.stringify(obj))
+    localStorage.setItem(obj.email, JSON.stringify(obj));
+
+    showNewUserOnScreen(obj);
+}
+
+function showNewUserOnScreen (user) {
+    const parentNode = document.getElementById('listOfUsers');
+    const childHTML = `<li> ${user.name} - ${user.email} </li>`
+
+    parentNode.innerHTML = parentNode.innerHTML+childHTML;
 }
