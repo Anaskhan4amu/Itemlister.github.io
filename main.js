c230ae14280ac88da79052f00575f334bb65cdae
Anas Khan <123639113+Anaskhan4amu@users.noreply.github.com>
2023-02-15 23:55:54 +0530

    task 14 edit

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function showNewUserOnScreen (user) {
     const parentNode = document.getElementById('listOfUsers');
     const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
         <button onclick=deleteUser('${user.email}')> Delete User </button> 
+        <button onclick=editUser('${user.email}','${user.name}')> Edit User </button>
         </li>`
 
     parentNode.innerHTML = parentNode.innerHTML+childHTML;
@@ -35,4 +36,12 @@ function removeUserFromScreen(emailId){
     const childNodeToBeDeleted = document.getElementById(emailId);
 
     parentNode.removeChild(childNodeToBeDeleted)
+}
+
+function editUser(emailId, name) {
+    document.getElementById('email').value=emailId;
+    document.getElementById('name').value=name;
+
+    deleteUser(emailId)
+
 }
